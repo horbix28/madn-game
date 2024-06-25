53cbd0b93c4408c2cefcc16f22fe2c72f838ebb8
horbix28 <60316568+horbix28@users.noreply.github.com>
2024-06-25 20:47:00 +0200

    Added display handling and required modules

--- physical/send_display_text.py
+++ physical/send_display_text.py
@@ -1,3 +1,9 @@
+from RPLCD.i2c import CharLCD
+
+def init_lcd():
+    lcd = CharLCD("PCF8574", 0x27, cols=16, rows=2, backlight_enabled=True)
+    return lcd
+
 if __name__ == "__main__":
     # manual test goes here
     pass
